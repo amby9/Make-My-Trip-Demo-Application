ac60fd7cf7bdbd26ab527c49daf3ccb974c6134d
Amber Srivastava <amber.srivastava@AmberSrivastavaMac.local>
2020-08-23 22:55:10 +0530

    refactored swagger

--- src/main/java/com/user/ledger/platform/controllers/UserController.java
+++ src/main/java/com/user/ledger/platform/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.user.ledger.platform.controllers;
 
+import com.user.ledger.platform.exceptions.APIExceptions;
 import com.user.ledger.platform.requests.UserNoteRequest;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -27,10 +28,10 @@ public class UserController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(code = Constants.HTTP_CREATED, message = Constants.HTTP_STATUS_CREATED, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_BAD_REQUEST, message = Constants.HTTP_STATUS_BAD_REQUEST, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_UNAUTHORIZED, message = Constants.HTTP_STATUS_UNAUTHORIZED, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_INTERNAL_SERVER_ERROR, message = Constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, response = Object.class)})
+            @ApiResponse(code = Constants.HTTP_CREATED, message = Constants.HTTP_STATUS_CREATED, response = GenericResponse.class),
+            @ApiResponse(code = Constants.HTTP_CODE_BAD_REQUEST, message = Constants.HTTP_STATUS_BAD_REQUEST, response = APIExceptions.class),
+            @ApiResponse(code = Constants.HTTP_CODE_UNAUTHORIZED, message = Constants.HTTP_STATUS_UNAUTHORIZED, response = APIExceptions.class),
+            @ApiResponse(code = Constants.HTTP_CODE_INTERNAL_SERVER_ERROR, message = Constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, response = APIExceptions.class)})
     @RequestMapping(value = "/app/user", method = RequestMethod.POST)
     public ResponseEntity<?> registerUser(@RequestBody UserRegistrationOrLoginRequest userRegistrationOrLoginRequest) {
         GenericResponse genericResponse = userService.registerUser(userRegistrationOrLoginRequest);
@@ -38,10 +39,10 @@ public class UserController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(code = Constants.HTTP_CODE_OK, message = Constants.HTTP_STATUS_OK, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_BAD_REQUEST, message = Constants.HTTP_STATUS_BAD_REQUEST, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_UNAUTHORIZED, message = Constants.HTTP_STATUS_UNAUTHORIZED, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_INTERNAL_SERVER_ERROR, message = Constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, response = Object.class)})
+            @ApiResponse(code = Constants.HTTP_CODE_OK, message = Constants.HTTP_STATUS_OK, response = GenericResponse.class),
+            @ApiResponse(code = Constants.HTTP_CODE_BAD_REQUEST, message = Constants.HTTP_STATUS_BAD_REQUEST, response = APIExceptions.class),
+            @ApiResponse(code = Constants.HTTP_CODE_UNAUTHORIZED, message = Constants.HTTP_STATUS_UNAUTHORIZED, response = APIExceptions.class),
+            @ApiResponse(code = Constants.HTTP_CODE_INTERNAL_SERVER_ERROR, message = Constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, response = APIExceptions.class)})
     @RequestMapping(value = "/app/user/auth", method = RequestMethod.POST)
     public ResponseEntity<?> loginUser(@RequestBody UserRegistrationOrLoginRequest userRegistrationOrLoginRequest) {
         GenericResponse genericResponse = userService.loginUser(userRegistrationOrLoginRequest);
@@ -49,10 +50,10 @@ public class UserController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(code = Constants.HTTP_CODE_OK, message = Constants.HTTP_STATUS_OK, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_BAD_REQUEST, message = Constants.HTTP_STATUS_BAD_REQUEST, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_UNAUTHORIZED, message = Constants.HTTP_STATUS_UNAUTHORIZED, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_INTERNAL_SERVER_ERROR, message = Constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, response = Object.class)})
+            @ApiResponse(code = Constants.HTTP_CODE_OK, message = Constants.HTTP_STATUS_OK, response = List.class),
+            @ApiResponse(code = Constants.HTTP_CODE_BAD_REQUEST, message = Constants.HTTP_STATUS_BAD_REQUEST, response = APIExceptions.class),
+            @ApiResponse(code = Constants.HTTP_CODE_UNAUTHORIZED, message = Constants.HTTP_STATUS_UNAUTHORIZED, response = APIExceptions.class),
+            @ApiResponse(code = Constants.HTTP_CODE_INTERNAL_SERVER_ERROR, message = Constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, response = APIExceptions.class)})
     @RequestMapping(value = "/app/sites/list", method = RequestMethod.GET)
     public ResponseEntity<?> getUserNotes(@RequestParam String userId) {
         List<String> userNotes = userService.getUserNotes(userId);
@@ -60,10 +61,10 @@ public class UserController {
     }
 
     @ApiResponses(value = {
-            @ApiResponse(code = Constants.HTTP_CODE_OK, message = Constants.HTTP_STATUS_OK, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_BAD_REQUEST, message = Constants.HTTP_STATUS_BAD_REQUEST, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_UNAUTHORIZED, message = Constants.HTTP_STATUS_UNAUTHORIZED, response = Object.class),
-            @ApiResponse(code = Constants.HTTP_CODE_INTERNAL_SERVER_ERROR, message = Constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, response = Object.class)})
+            @ApiResponse(code = Constants.HTTP_CODE_OK, message = Constants.HTTP_STATUS_OK, response = GenericResponse.class),
+            @ApiResponse(code = Constants.HTTP_CODE_BAD_REQUEST, message = Constants.HTTP_STATUS_BAD_REQUEST, response = APIExceptions.class),
+            @ApiResponse(code = Constants.HTTP_CODE_UNAUTHORIZED, message = Constants.HTTP_STATUS_UNAUTHORIZED, response = APIExceptions.class),
+            @ApiResponse(code = Constants.HTTP_CODE_INTERNAL_SERVER_ERROR, message = Constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, response = APIExceptions.class)})
     @RequestMapping(value = "/app/sites", method = RequestMethod.POST)
     public ResponseEntity<?> addUserNote(@RequestParam String userId, @RequestBody UserNoteRequest userNoteRequest) {
         GenericResponse genericResponse = userService.addUserNote(userId, userNoteRequest);
